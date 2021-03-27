7e8568a11d50c684046040a8d20ccb83d57804bc
engin-eer <engin.aydgdu@gmail.com>
2021-03-27 23:51:51 +0300

    first commit

---
+++ src/app/models/car.ts
@@ -0,0 +1,14 @@
+//import { CarImage } from "./carImage";
+
+export interface Car{
+    carId:number;  
+    colorId:number;
+    brandId:number;    
+    brandName:string;
+    colorName:string;
+    imagePath:string;
+    modelYear:number;
+    dailyPrice:number;
+    description:string;
+    //carImages : CarImage[];
+  }
